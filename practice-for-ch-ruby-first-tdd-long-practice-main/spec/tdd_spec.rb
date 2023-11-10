8b8827fa0c4c659977a6fea34cdb1ee63b667507
JhonJSC182 <jhonjsc182@hotmail.com>
2023-11-10 15:34:08 -0500

    finished  two_sum

--- practice-for-ch-ruby-first-tdd-long-practice-main/spec/tdd_spec.rb
+++ practice-for-ch-ruby-first-tdd-long-practice-main/spec/tdd_spec.rb
@@ -12,8 +12,23 @@ describe Array do
       array = [1, 2, 4, 1]
       expect(array).to_not receive(:uniq)
       array.my_uniq
-    end 
-
+    end
   end 
+
+  describe '#two_sum' do
+    context "when the array contains numbers as elements" do
+      it "#two_sum should return all pairs of positions, where the elements at those positions sum to 0" do
+        expect([-1, 0, 2, -2, 1].two_sum).to eq([[0, 4], [2, 3]])
+        expect([5, 3, -5, 4, -3].two_sum).to eq([[0, 2], [1, 4]])
+      end
+    end
+
+    context "when the array is empty" do 
+      it "should return nil" do
+        expect([].two_sum).to eq(nil)
+      end
+    end
+
+  end
 
 end 
