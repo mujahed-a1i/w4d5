21bdca717a4b54c774a67909df37b17cfe1d0d97
Mujahed Ali <mujahed97@gmail.com>
2023-11-10 15:48:38 -0500

    finished two_sum"

--- practice-for-ch-ruby-first-tdd-long-practice-main/lib/tdd.rb
+++ practice-for-ch-ruby-first-tdd-long-practice-main/lib/tdd.rb
@@ -4,6 +4,20 @@ class Array
         self.each { |ele| array << ele if !array.include?(ele)}
         return array
     end 
+
+    def two_sum 
+        return nil if self.empty?
+
+        array = []
+        (0...self.length).each do |idx|
+            (idx+1...self.length).each do |idx2|
+                array << [idx, idx2] if self[idx] + self[idx2] == 0
+            end
+        end
+
+        return array
+    end 
 end 
 
 p [5, 6, 5, 6, 9, 8, 7].my_uniq
+p [-1, 0, 2, -2, 1].two_sum
